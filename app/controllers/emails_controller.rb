6cc171e635201426bdcaefc72d71f43b7bd3e730
Mashallah Behbehani <mkbehbehani@gmail.com>
2013-01-21 00:34:21 -0600

    Fixes authentication on new

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -27,7 +27,6 @@ class EmailsController < ApplicationController
   def new
 
     @email = Email.new
-    before_filter :authenticate_user!
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @email }
