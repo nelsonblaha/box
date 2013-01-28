75d75b0e699d1a5b7598b96d0ccf361698138682
Mashalla <mashalla@mashalla-virtual-machine.(none)>
2013-01-28 09:59:24 -0600

    pg,sqlite gemfile mod for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130124011132) do
+ActiveRecord::Schema.define(:version => 20130128144841) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",              :default => "", :null => false
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(:version => 20130124011132) do
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
     t.boolean  "admin",                  :default => false
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
