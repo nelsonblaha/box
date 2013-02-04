f810d0f845b49892ba3cad1aba75ee3427cc48d3
Nelson Blaha <nelson.blaha@gmail.com>
2013-02-03 18:24:47 -0600

    cleaned up email model to remove old-style associations

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,8 +1,6 @@
 class Email < ActiveRecord::Base
   attr_accessible :content, :date_created, :id, :rank, :title
-  belongs_to :user
-  #dependent destroy causes an email's votes to be burned with it on the funeral pyre, Conan-style
-  	has_many :votes, dependent: :destroy
-  
+
+  has_many :votes, dependent: :destroy
   has_many :users, through: :votes
 end
